e7f6608b80911e11d69cf3b055e886f14427399e
EngrJ21 <cpe.jianjayne@gmail.com>
2024-11-04 17:01:49 +0800

    Fix price formatting and radio selectors

---
+++ javascript-amazon-project-main/scripts/utils/dist/money.dev.js
@@ -0,0 +1,11 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.formatCurrency = formatCurrency;
+
+function formatCurrency(priceCents) {
+  return (priceCents / 100).toFixed(2);
+}
+//# sourceMappingURL=money.dev.js.map
