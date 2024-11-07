5f37cc550df0b0900fb81e5a836df23f0dc153ea
EngrJ21 <cpe.jianjayne@gmail.com>
2024-11-07 21:07:36 +0800

    Create class version of cart

--- javascript-amazon-project-main/scripts/dist/checkout.dev.js
+++ javascript-amazon-project-main/scripts/dist/checkout.dev.js
@@ -4,7 +4,7 @@ var _orderSummary = require("./checkout/orderSummary.js");
 
 var _paymentSummary = require("./checkout/paymentSummary.js");
 
-require("../data/cart-oop.js");
+require("../data/cart-class.js");
 
 (0, _orderSummary.renderOrderSummary)();
 (0, _paymentSummary.renderPaymentSummary)();
