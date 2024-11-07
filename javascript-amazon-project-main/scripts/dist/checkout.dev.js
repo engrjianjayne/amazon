8e1e02b4b627346f617117f27fbb4a38317cce44
EngrJ21 <cpe.jianjayne@gmail.com>
2024-11-07 21:41:08 +0800

    Convert products into classes

--- javascript-amazon-project-main/scripts/dist/checkout.dev.js
+++ javascript-amazon-project-main/scripts/dist/checkout.dev.js
@@ -4,8 +4,6 @@ var _orderSummary = require("./checkout/orderSummary.js");
 
 var _paymentSummary = require("./checkout/paymentSummary.js");
 
-require("../data/cart-class.js");
-
 (0, _orderSummary.renderOrderSummary)();
 (0, _paymentSummary.renderPaymentSummary)();
 //# sourceMappingURL=checkout.dev.js.map
