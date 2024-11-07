b769f1cc1b2a1063439147f32c2f7e45d45538bd
EngrJ21 <cpe.jianjayne@gmail.com>
2024-11-07 08:07:41 +0800

    Rename tests-jasmine folder to tests

--- javascript-amazon-project-main/tests/dist/moneyTest.dev.js
+++ javascript-amazon-project-main/tests/dist/moneyTest.dev.js
@@ -2,28 +2,15 @@
 
 var _money = require("../scripts/utils/money.js");
 
-console.log('test suite: formatCurrency');
-console.log('converts cents into dollars');
-
-if ((0, _money.formatCurrency)(2095) === '20.95') {
-  console.log('passed');
-} else {
-  console.log('failed');
-}
-
-console.log('works with 0');
-
-if ((0, _money.formatCurrency)(0) === '0.00') {
-  console.log('passed');
-} else {
-  console.log('failed');
-}
-
-console.log('round up to the nearest cent');
-
-if ((0, _money.formatCurrency)(2000.5) === '20.01') {
-  console.log('passed');
-} else {
-  console.log('failed');
-}
+describe('test suite: formatCurrency', function () {
+  it('converts cents into dollars', function () {
+    expect((0, _money.formatCurrency)(2095)).toEqual('20.95');
+  });
+  it('works with 0', function () {
+    expect((0, _money.formatCurrency)(0)).toEqual('0.00');
+  });
+  it('round up to the nearest cent', function () {
+    expect((0, _money.formatCurrency)(2000.5)).toEqual('20.01');
+  });
+});
 //# sourceMappingURL=moneyTest.dev.js.map
