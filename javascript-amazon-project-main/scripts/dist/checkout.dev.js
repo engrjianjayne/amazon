b61a03440145c500dac97f89d8d89b2ff9487f6b
EngrJ21 <cpe.jianjayne@gmail.com>
2024-11-09 04:25:09 +0800

    Add backend practrice file

--- javascript-amazon-project-main/scripts/dist/checkout.dev.js
+++ javascript-amazon-project-main/scripts/dist/checkout.dev.js
@@ -4,6 +4,8 @@ var _orderSummary = require("./checkout/orderSummary.js");
 
 var _paymentSummary = require("./checkout/paymentSummary.js");
 
+require("../data/backend-practice.js");
+
 (0, _orderSummary.renderOrderSummary)();
 (0, _paymentSummary.renderPaymentSummary)();
 //# sourceMappingURL=checkout.dev.js.map
