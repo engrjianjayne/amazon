de48a8777c06a9b873ade76768db33b8a1615128
EngrJ21 <cpe.jianjayne@gmail.com>
2024-11-09 21:06:27 +0800

    Convert from promises to async await

--- javascript-amazon-project-main/scripts/dist/checkout.dev.js
+++ javascript-amazon-project-main/scripts/dist/checkout.dev.js
@@ -10,15 +10,53 @@ var _cart = require("../data/cart.js");
 
 // import '../data/cart-class.js';
 // import '../data/backend-practice.js';
-Promise.all([(0, _products.loadProductsFetch)(), new Promise(function (resolve) {
-  (0, _cart.loadCart)(function () {
-    resolve();
+function loadPage() {
+  var value;
+  return regeneratorRuntime.async(function loadPage$(_context) {
+    while (1) {
+      switch (_context.prev = _context.next) {
+        case 0:
+          _context.next = 2;
+          return regeneratorRuntime.awrap((0, _products.loadProductsFetch)());
+
+        case 2:
+          _context.next = 4;
+          return regeneratorRuntime.awrap(new Promise(function (resolve) {
+            (0, _cart.loadCart)(function () {
+              resolve('value');
+            });
+          }));
+
+        case 4:
+          value = _context.sent;
+          (0, _orderSummary.renderOrderSummary)();
+          (0, _paymentSummary.renderPaymentSummary)();
+
+        case 7:
+        case "end":
+          return _context.stop();
+      }
+    }
   });
-})]).then(function (values) {
+}
+
+loadPage();
+/*
+Promise.all([
+  loadProductsFetch(),
+  new Promise((resolve) => {
+    loadCart(() => {
+      resolve();
+    });
+  })
+
+]).then((values) =>{
   console.log(values);
-  (0, _orderSummary.renderOrderSummary)();
-  (0, _paymentSummary.renderPaymentSummary)();
+  renderOrderSummary();
+  renderPaymentSummary();
 });
+*/
+
 /*
 new Promise((resolve) => {
   loadProducts(() => {
